d37ed33c3337ab7b649223208b9886c05f9f3a04
emrekabakci <emrekabakci.2@icloud.com>
2023-12-17 23:43:25 +0300

    Comment added.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Variables defined here.
         int enteredNum, total = 0;
+
+        //Scanner created.
         Scanner input = new Scanner(System.in);
 
+        //Data retrieved from user.
         System.out.print("Please Enter a Number: ");
         enteredNum = input.nextInt();
 
+        //Sum of digits Calculated.
         while (enteredNum != 0) {
             total += enteredNum % 10;
             enteredNum /= 10;
         }
 
+        //Result printed.
         System.out.println("result: " + total);
     }
 }
